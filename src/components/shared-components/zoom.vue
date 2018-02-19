1d46843e01b4c60b2cac7dbcbe74c55f85e7e953
mygumi <mygumi@vtouch.kr>
2018-02-19 20:06:14 +0900

    detail image rate issue

--- src/components/shared-components/zoom.vue
+++ src/components/shared-components/zoom.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="zoom-container">
     <div class="zoom-card">
-      <span class="cancel" @click="cancel">
+      <p class="cancel" @click="cancel">
         <svg x="0px" y="0px" width="24px" height="24px" viewBox="0 0 510 510" style="enable-background:new 0 0 24 24;" xml:space="preserve"><path d="M405.6,69.6C360.7,24.7,301.1,0,237.6,0s-123.1,24.7-168,69.6S0,174.1,0,237.6s24.7,123.1,69.6,168s104.5,69.6,168,69.6 s123.1-24.7,168-69.6s69.6-104.5,69.6-168S450.5,114.5,405.6,69.6z M386.5,386.5c-39.8,39.8-92.7,61.7-148.9,61.7 s-109.1-21.9-148.9-61.7c-82.1-82.1-82.1-215.7,0-297.8C128.5,48.9,181.4,27,237.6,27s109.1,21.9,148.9,61.7 C468.6,170.8,468.6,304.4,386.5,386.5z"/>
           <path d="M342.3,132.9c-5.3-5.3-13.8-5.3-19.1,0l-85.6,85.6L152,132.9c-5.3-5.3-13.8-5.3-19.1,0c-5.3,5.3-5.3,13.8,0,19.1 l85.6,85.6l-85.6,85.6c-5.3,5.3-5.3,13.8,0,19.1c2.6,2.6,6.1,4,9.5,4s6.9-1.3,9.5-4l85.6-85.6l85.6,85.6c2.6,2.6,6.1,4,9.5,4 c3.5,0,6.9-1.3,9.5-4c5.3-5.3,5.3-13.8,0-19.1l-85.4-85.6l85.6-85.6C347.6,146.7,347.6,138.2,342.3,132.9z"/>
         </svg>
-      </span>
-      <img v-if="!largeImage" :src="smallImage"/>
-      <img v-if="largeImage" :src="largeImage"/>
+      </p>
+      <img :class="{hide: largeImage}" class="small-img" :src="smallImage"/>
+      <img :class="{loaded: largeImage}" :src="largeImage"/>
     </div>
   </div>
 </template>
@@ -75,7 +75,7 @@ export default {
     z-index: 9999;
     @include respond-to($large-desktop) {
       width: 30%;
-      height: auto;
+      height: 100%;
       transform: translate(-50%, -50%);
       top: 50%;
       left: 50%;
@@ -85,21 +85,39 @@ export default {
     .zoom-card {
       position: relative;
       width: 100%;
+      height: 70%;
       background-color: #fff;
       border-radius: 5px;
       box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
       img {
         padding: 0rem 2rem 2rem 2rem;
         width: 100%;
-        height: auto;
-        @include respond-to($large-desktop) {
+        height: 90%;
+        opacity: 0;
+        transition: opacity 1s linear;
+      }
+      .small-img {
+        opacity: 1;
+        filter: blur(10px);
+      }
+      .loaded {
+        opacity: 1;
+      }
+      .hide {
+        position: absolute;
+        top: 40px;
+        left: 0;
+        opacity: 0;
+      }
+      .cancel {
+        height: 2.5rem;
+        svg {
           width: 100%;
+          height: 100%;
+          z-index: 9999;
         }
       }
-      svg {
-        width: 2.5rem;
-        height: 2.5rem;
-      }
+      
     }
   }
 </style>
