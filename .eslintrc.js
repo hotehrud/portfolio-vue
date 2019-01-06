6b8197567a330f454d8ed5ae29207d55ee8839f9
mygumi <hotehrud@naver.com>
2019-01-06 14:15:45 +0900

    .file update

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,24 +2,27 @@
 
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
+  // parser: "babel-eslint",
   parserOptions: {
-    sourceType: 'module'
+    parser: "babel-eslint",
+    ecmaVersion: 2017
+    // "sourceType": "module"
   },
   env: {
-    browser: true,
+    browser: true
   },
-  // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-  extends: 'standard',
-  // required to lint *.vue files
-  plugins: [
-    'html'
+  // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+  // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+  extends: [
+    "plugin:vue/recommended",
+    "plugin:prettier/recommended",
+    "eslint:recommended"
   ],
+  // required to lint *.vue files
+  plugins: ["vue", "html"],
   // add your custom rules here
   rules: {
-    // allow async-await
-    'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
   }
-}
+};
